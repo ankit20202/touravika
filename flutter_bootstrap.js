5e364f23e621dddcd813d22c5342d034da722bd3
ankit <ankitsuvagiya.cloudus23@gmail.com>
2024-10-14 11:01:02 +0530

    commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3439289959"
+    serviceWorkerVersion: "3191846838"
   }
 });
