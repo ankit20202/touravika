3f8d8398346230df34b4d9477d3cf89de1f45ad9
ankit <ankitsuvagiya.cloudus23@gmail.com>
2024-10-18 15:36:54 +0530

    build live

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "6fece0e80df56aa6a268a64b1aafab58",
+const RESOURCES = {"flutter_bootstrap.js": "d38797b5031c6117094999c813b4a9c1",
 "version.json": "31fac307d29ff9f6b85cad121b149f22",
 "index.html": "1431b36134fc33a95a1496d4d1c80393",
 "/": "1431b36134fc33a95a1496d4d1c80393",
-"main.dart.js": "8612d2fd96a71b6a4b41400a84d9fae4",
+"main.dart.js": "be952756b383ec1b271d24914b27bb47",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
