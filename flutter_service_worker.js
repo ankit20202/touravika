17116cc2577807e7ea38cfd2258f083f20382e65
dreamworld <hardikbeladiya.cloudus22@gmail.com>
2024-11-08 19:04:57 +0530

    build live

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "71fd928fe78011bd72a8d652ce7140e4",
+const RESOURCES = {"flutter_bootstrap.js": "05c43a761d44277b25daaeeb8e19d755",
 "version.json": "31fac307d29ff9f6b85cad121b149f22",
 "index.html": "1431b36134fc33a95a1496d4d1c80393",
 "/": "1431b36134fc33a95a1496d4d1c80393",
-"main.dart.js": "b8b5f0404945548914ac56b8a21fbed4",
+"main.dart.js": "bd7785e97e30d409a1d5ac3f3680c919",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "91f16c2dccd47cccefc7ed5118a5765f",
 "icons/Icon-192.png": "91f16c2dccd47cccefc7ed5118a5765f",
